a72a90e631ecddba872d37ba56d561f12dd28034
Irvin Lim <irvinlimweiquan@gmail.com>
2018-02-13 05:01:28 +0800

    Add styling of HTML/CSS and add toggle program button

--- src/ui.js
+++ src/ui.js
@@ -2,6 +2,25 @@ var selection = 0;
 var filtering = 0;
 var counter = 0;
 
+// Toggles the status of the program.
+function toggleStatus(el) {
+    if (el.value === 'Stopped') {
+        // Start the program.
+        el.value = 'Started';
+        el.classList.remove('is-danger');
+        el.classList.add('is-success');
+        renderLoop();
+    } else {
+        // Stop the program.
+        el.value = 'Stopped';
+        el.classList.add('is-danger');
+        el.classList.remove('is-success');
+        window.cancelAnimationFrame(renderLoopRequestId);
+        renderLoopRequestId = undefined;
+    }
+}
+
+// Changes the CPU/GPU state.
 function change(el) {
     if (el.value === 'Using CPU') {
         selection = 1;
@@ -12,6 +31,7 @@ function change(el) {
     }
 }
 
+// Toggles the filter.
 function changeFilter(el) {
     if (el.value === 'Filtering') {
         filtering = 1;
