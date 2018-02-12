ac0763d0280235459376b08fa794fe8c29053b12
Irvin Lim <irvinlimweiquan@gmail.com>
2018-02-13 05:02:16 +0800

    Formatting of provided source files

--- src/js/decls.js
+++ src/js/decls.js
@@ -1,21 +1,30 @@
+function Enum(constantsList) {
+    for (var i in constantsList) {
+        this[constantsList[i]] = i;
+    }
+}
 
-   function Enum(constantsList) {
-      for (var i in constantsList) {
-         this[constantsList[i]] = i;
-      }
-   }
+var ObjTyp = new Enum([
+    'EMPTY',
+    'SPHERE',
+    'CUBOID',
+    'CYLINDER',
+    'CONE',
+    'TRIANGLE',
+]);
 
-   var ObjTyp = new Enum(['EMPTY', 'SPHERE', 'CUBOID', 'CYLINDER', 'CONE', 'TRIANGLE']);
-
-   var fps = { startTime : 0, frameNumber : 0,
-               getFPS : function() {
-                  this.frameNumber++;
-                  var d = new Date().getTime(), currentTime = ( d - this.startTime ) / 1000, result = Math.floor( ( (this.frameNumber*10) / currentTime ) );
-                  if( currentTime > 1 ) {
-                     this.startTime = new Date().getTime();
-                     this.frameNumber = 0;
-                  }
-                  return result/10.0;
-               }
-             };
-
+var fps = {
+    startTime: 0,
+    frameNumber: 0,
+    getFPS: function() {
+        this.frameNumber++;
+        var d = new Date().getTime(),
+            currentTime = (d - this.startTime) / 1000,
+            result = Math.floor(this.frameNumber * 10 / currentTime);
+        if (currentTime > 1) {
+            this.startTime = new Date().getTime();
+            this.frameNumber = 0;
+        }
+        return result / 10.0;
+    },
+};
